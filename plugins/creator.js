6642b184895d50964b0d4c050f383ddf5fc0a106
Haii my name is adi <80912288+BotByAdii@users.noreply.github.com>
2021-05-27 23:04:16 +0700

    Add files via upload

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 let handler = function (m) {
-  this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
+  this.sendContact(m.chat, '0', 'MARK', m)
   //plsss do not change this, jika mau add boleh tapi jangan rubah
-  this.sendContact(m.chat, '6281390658325', 'Owner Metro Bot :)', m)
+  this.sendContact(m.chat, '6289504585790', 'Owner kuu><', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
